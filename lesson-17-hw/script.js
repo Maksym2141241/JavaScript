f6aeb0275b3862e6aec92d708213b7594a1cf821
Maksym2141241 <nerro2008@gmail.com>
2019-04-03 19:50:27 +0300

    lesson-17-hw

---
+++ lesson-17-hw/script.js
@@ -0,0 +1,37 @@
+//1
+var sec = 1;
+var minute = 60 * sec;
+var hour = 60 * minute;
+
+console.log(3 * hour);
+
+//2
+var num = 1;
+num = (num + 12 -  14) * 5 / 7;
+
+console.log(num);
+
+
+//3
+var a = 10;
+var b = 2; 
+var c = 5;
+
+console.log(a + b + c);
+
+//4
+a = 17;
+b = 10;
+c = a - b;
+var d = 7;
+var result = c + d;
+
+console.log(result);
+//5
+var name = ('Hello, Maksym!');
+
+alert(name);
+
+
+
+
---
+++ lesson-17-js/script.js
@@ -0,0 +1,44 @@
+var COLOR_RED = '#F00';
+
+var flower = 'roza';
+
+flower = 'neroza';
+
+var user = 'Oleg',  age = 25, messege = 'Hello';
+
+var emptyvar;
+
+emptyvar = flower;
+
+var dogName ='Sharik';
+
+//Числовое
+var number = 122;
+//Infinity
+console.log( 1 / 0);
+
+//NaN
+console.log('нечисло' /5);
+
+var str = '<div class=""> </div>';
+
+//true false 1 0
+var  cheched = true;
+
+//null
+var age = null;
+
+//undefined
+var x;
+
+var user = { name: 'Vasya', age: 22};
+
+var user = { name: 'Vasya', age: { age1: 15, age2: 22}};
+
+
+
+
+
+console.log(typeof flower);
+
+console.log(x);
