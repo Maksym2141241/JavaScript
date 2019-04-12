4c54e8b8a45ed2dc61a39e438716c859bc692154
Maksym2141241 <nerro2008@gmail.com>
2019-04-12 17:00:38 +0300

    lesson-19-hw

--- lesson-19-hw/script.js
+++ lesson-19-hw/script.js
@@ -1,4 +1,48 @@
 "use strict"
 
 
-function isNumberInRage
+console.log('-------1---------');
+function isNumberInRange(number) {
+    console.log(number > 0 && number < 10);
+}
+
+isNumberInRange(2);
+
+
+
+console.log('-------2---------');
+function isEven(even) {
+    console.log(even % 2 == 0);
+}
+
+isEven(3);
+
+
+console.log('-------3---------');
+var value = +prompt('value', '');
+switch (value) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+}
+
+
+console.log('-------4---------');
+function min(a, b) {
+    if (a < b) {
+        console.log(a);
+        return a;
+    } else {
+        console.log(b);
+        return b;
+    }
+}
+
+min(32, 18);
