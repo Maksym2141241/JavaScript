21e707011f29fc0641416690643e0157c1af3a04
Maksym2141241 <nerro2008@gmail.com>
2019-04-15 12:33:57 +0300

    lesson-20-hw

---
+++ lesson-20-hw/script.js
@@ -0,0 +1,25 @@
+"use strict"
+//-------------------------1---------------------------
+function fib(n) {
+    return n > 1 ? fib(n - 1) + fib(n - 2) : n;
+}
+
+
+console.log(fib(10));
+
+
+//-------------------------2---------------------------
+function delWhile(n) {
+    var num = 0;
+    while(n>50){
+        num++;
+        n/=2;
+    }
+    return num;
+}
+
+
+var num = delWhile(1000);
+
+console.log(num);
+
